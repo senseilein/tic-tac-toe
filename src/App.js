ad97b749104dca3c20fb8b0a91631192e9f66cc5
Senseilein <73607360+senseilein@users.noreply.github.com>
2024-07-22 00:17:45 +0100

    Implement handleClick() in Board component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import { useState } from "react";
 
-const Square = ({ value }) => {
-  // const [value, setValue] = useState(null);
-
-  // const handleClick = () => {
-  //   setValue("X");
-  // };
-
-  // return (
-  //   <button className="square" onClick={handleClick}>
-  //     {value}
-  //   </button>
-  // );
-
-  return <button className="square">{value}</button>;
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 };
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  const handleClick = (i) => {
+    //create a copy of the squares array
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        {/* <Square value={squares[0]} onSquareClick={handleClick(0)} />
+        Here is why this doesn’t work. The handleClick(0) call will be a part of rendering the board component. Because handleClick(0) alters the state of the board component by calling setSquares, your entire board component will be re-rendered again. But this runs handleClick(0) again, leading to an infinite loop: */}
+
+        {/* Notice the new () => syntax. Here, () => handleClick(0) is an arrow function, which is a shorter way to define functions. When the square is clicked, the code after the => “arrow” will run, calling handleClick(0). */}
+
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
@@ -42,4 +47,10 @@ export default function Board() {
 
 /*
 To collect data from multiple children, or to have two child components communicate with each other, declare the shared state in their parent component instead. The parent component can pass that state back down to the children via props. This keeps the child components in sync with each other and with their parent.
+
+------
+
+ Since state is private to a component that defines it, you cannot update the Board’s state directly from Square.
+
+Instead, you’ll pass down a function from the Board component to the Square component, and you’ll have Square call that function when a square is clicked
 */
